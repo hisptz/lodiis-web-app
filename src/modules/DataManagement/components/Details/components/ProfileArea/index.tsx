99c787a3ff3d793e22d1098e02e9168f8ca596a8
gnnko <nnkogift@hotmail.com>
2023-01-23 10:39:49 +0300

    fix(data management): Refactor
    - Refactored details container
    
    Signed-off-by: gnnko <nnkogift@hotmail.com>

--- src/modules/DataManagement/components/Details/components/ProfileArea/index.tsx
+++ src/modules/DataManagement/components/Details/components/ProfileArea/index.tsx
@@ -1,30 +1,27 @@
 import React from "react";
-import classes from "../../Details.module.css"
 import i18n from '@dhis2/d2-i18n';
-import {Divider} from '@dhis2/ui'
 import {DetailArea} from "../DetailArea";
+import {AreaContainer} from "../AreaContainer";
 
 export function ProfileArea() {
 
 
     return (
         <div className="column gap-16 w-100 h-100">
-            <div className={classes['area-container']}>
-                <h3 className={classes['area-header']}>{i18n.t("Profile")}</h3>
-                <Divider dense/>
+
+
+            <AreaContainer heading={i18n.t("Profile")}>
                 <div className="column gap-8">
                     <DetailArea header={i18n.t("First name")} value={"Elena Mikala"}/>
                     <DetailArea header={i18n.t("First Name")} value={"Elena Mikala"}/>
                 </div>
-            </div>
-            <div className={classes['area-container']}>
-                <h3 className={classes['area-header']}>{i18n.t("Enrollment")}</h3>
-                <Divider dense/>
-            </div>
-            <div className={classes['area-container']}>
-                <h3 className={classes['area-header']}>{i18n.t("Relationships")}</h3>
-                <Divider dense/>
-            </div>
+            </AreaContainer>
+            <AreaContainer heading={i18n.t("Enrollment")}>
+
+            </AreaContainer>
+            <AreaContainer heading={i18n.t("Relationships")}>
+
+            </AreaContainer>
         </div>
     )
 }
--- src/modules/DataManagement/components/Details/components/StagesArea/index.tsx
+++ src/modules/DataManagement/components/Details/components/StagesArea/index.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import classes from "../../Details.module.css"
 import i18n from "@dhis2/d2-i18n";
-import {Divider} from "@dhis2/ui";
+import {AreaContainer} from "../AreaContainer";
 
 export function StagesArea() {
 
     return (
-        <div className={`${classes['area-container']}`}>
-            <h3 className={classes['area-header']}>{i18n.t("Stages and events")}</h3>
-            <Divider margin={"2px 0"} dense/>
-        </div>
+        <AreaContainer heading={i18n.t("Stages and events")}>
+
+        </AreaContainer>
     )
 }
