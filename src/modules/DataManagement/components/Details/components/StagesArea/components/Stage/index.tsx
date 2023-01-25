ac916f6be9382070318e764a145bcfedeb0e8fb4
gnnko <nnkogift@hotmail.com>
2023-01-25 12:33:46 +0300

    feat(data management): Implementation
    - Implemented empty list view for stages
    - Updated stages header to services
    
    Signed-off-by: gnnko <nnkogift@hotmail.com>

--- src/modules/DataManagement/components/Details/components/StagesArea/components/Stage/index.tsx
+++ src/modules/DataManagement/components/Details/components/StagesArea/components/Stage/index.tsx
@@ -3,11 +3,18 @@ import {ProgramStage} from "@hisptz/dhis2-utils";
 import i18n from '@dhis2/d2-i18n';
 import {useData} from "../../../../hooks/data";
 import {Event as DHIS2Event} from "@hisptz/dhis2-utils/build/types/interfaces/dhis2/tracker";
-import {CustomDataTable} from "@hisptz/dhis2-ui";
+import {CustomDataTable, useConfirmDialog} from "@hisptz/dhis2-ui";
 import {fromPairs} from "lodash";
 import {DateTime} from "luxon";
 
-import {Tag} from '@dhis2/ui'
+import {Button, ButtonStrip, IconDelete24, IconEdit24, Tag} from '@dhis2/ui'
+import {useAlert, useDataMutation} from "@dhis2/app-runtime";
+
+const eventDeleteMutation: any = {
+    resource: "events",
+    type: "delete",
+    id: ({id}: any) => id
+}
 
 
 const columns = [
@@ -43,13 +50,63 @@ const columns = [
 ]
 
 export function Stage({stage}: { stage: ProgramStage }) {
-    const {profileData: profile} = useData();
+    const {profileData: profile, refetch} = useData();
     const events = profile?.getEvents(stage.id) ?? [];
 
+    const {show, hide} = useAlert(({message}) => message, ({type}) => ({...type, duration: 3000}))
+
+    const {confirm,} = useConfirmDialog();
+    const [deleteEvent] = useDataMutation(eventDeleteMutation, {
+        onComplete: () => {
+            show({message: i18n.t("Event deleted successfully"), type: {success: true}});
+            refetch();
+        },
+        onError: (error) => {
+            show({message: i18n.t(error.details.message ?? error.message), type: {critical: true}});
+            setTimeout(() => hide(), 5000)
+        }
+    })
+
+    const onEdit = (event: DHIS2Event) => () => {
+
+    }
+
+    const onDelete = (event: DHIS2Event) => () => {
+        confirm({
+            title: i18n.t("Delete event"),
+            message: i18n.t("Are you sure you want to delete this event"),
+            loadingText: i18n.t("Deleting"),
+            confirmButtonText: i18n.t("Delete"),
+
+            onCancel: () => {
+
+            },
+            onConfirm: async () => {
+                await deleteEvent({
+                    id: event.event
+                })
+            }
+        })
+    }
+
+    const sanitizedColumns = [...columns, {
+        label: i18n.t("Actions"),
+        key: "actions",
+        get: (event: DHIS2Event) => {
+            return (
+                <ButtonStrip>
+                    <Button small onClick={onEdit(event)} icon={<IconEdit24/>}/>
+                    <Button small destructive onClick={onDelete(event)} icon={<IconDelete24/>}/>
+                </ButtonStrip>
+            )
+        }
+    }]
+
     const rows = events.map(event => ({
-        ...fromPairs(columns.map(column => ([column.key, column.get(event)]))),
+        ...fromPairs(sanitizedColumns.map(column => ([column.key, column.get(event)]))),
         id: event.event
     }));
+
 
     return (<div className="column ">
         <div style={{alignItems: 'flex-end'}} className="row gap-16">
@@ -61,7 +118,7 @@ export function Stage({stage}: { stage: ProgramStage }) {
             }
         </div>
         <div className="">
-            <CustomDataTable tableProps={{scrollHeight: "500px"}} rows={rows} columns={columns}/>
+            <CustomDataTable tableProps={{scrollHeight: "320px"}} rows={rows} columns={sanitizedColumns}/>
         </div>
     </div>)
 }
--- src/modules/DataManagement/components/Details/components/StagesArea/index.tsx
+++ src/modules/DataManagement/components/Details/components/StagesArea/index.tsx
@@ -3,20 +3,30 @@ import i18n from "@dhis2/d2-i18n";
 import {AreaContainer} from "../AreaContainer";
 import {useData} from "../../hooks/data";
 import {Stage} from "./components/Stage";
+import {isEmpty} from "lodash";
+import EmptyList from "../../../../../../shared/components/EmptyList";
 
 
 export function StagesArea() {
     const {profileData: profile} = useData();
 
+    const stages = profile?.getProgramStages();
 
     return (
-        <AreaContainer heading={i18n.t("Stages and events")}>
-            <div style={{padding: "0 8px"}} className="column gap-16">
-                {
-                    profile?.getProgramStages()?.map(programStage => (
-                        <Stage key={`${programStage.id}-area`} stage={programStage}/>))
-                }
-            </div>
+        <AreaContainer heading={i18n.t("Services")}>
+            {
+                isEmpty(stages) && (<div className="column center align-center" style={{minHeight: "400px"}}>
+                    <EmptyList message={i18n.t("There are no services provided for this beneficiary")}/>
+                </div>)
+            }
+            {
+                !isEmpty(stages) && (<div style={{padding: "0 8px"}} className="column gap-16">
+                    {
+                        stages?.map(programStage => (
+                            <Stage key={`${programStage.id}-area`} stage={programStage}/>))
+                    }
+                </div>)
+            }
         </AreaContainer>
     )
 }
