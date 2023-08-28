0856791eb2599a8e7c19a26ce0c11691ef3a2944
Davis799 <davistibz@gmail.com>
2023-08-28 21:28:33 +0300

    fix(yarn.lock): fixes
    Fix dependencies issue where it found 2 versions of 'styled-jsx' in yarn.lock: 4.0.1, 5.1.2

--- src/modules/DataManagement/components/Table/index.tsx
+++ src/modules/DataManagement/components/Table/index.tsx
@@ -1,79 +1,106 @@
-import React from "react";
-import {useTableData} from "./hooks/data";
-import {CustomDataTable} from "@hisptz/dhis2-ui";
-import {ErrorBoundary} from "react-error-boundary";
-import ErrorFallback from "../../../../shared/components/ErrorFallback";
-import {useDimension} from "../../../../shared/hooks/dimension";
-import {useElementSize} from "usehooks-ts";
-import {CircularLoader} from "@dhis2/ui";
-import {ErrorThrower} from "../../../../shared/components/ErrorThrower";
-import i18n from '@dhis2/d2-i18n';
-import {isEmpty} from "lodash";
-import EmptyList from "../../../../shared/components/EmptyList";
-import {useNavigate} from "react-router-dom";
+import React from "react"
+import { useTableData } from "./hooks/data"
+import { CustomDataTable } from "@hisptz/dhis2-ui"
+import { ErrorBoundary } from "react-error-boundary"
+import ErrorFallback from "../../../../shared/components/ErrorFallback"
+import { useDimension } from "../../../../shared/hooks/dimension"
+import { useElementSize } from "usehooks-ts"
+import { CircularLoader } from "@dhis2/ui"
+import { ErrorThrower } from "../../../../shared/components/ErrorThrower"
+import i18n from "@dhis2/d2-i18n"
+import { isEmpty } from "lodash"
+import EmptyList from "../../../../shared/components/EmptyList"
+import { useNavigate } from "react-router-dom"
 
 export function ErrorBoundedTable() {
-    const {orgUnits, program, periods} = useDimension();
-    const [ref, {height}] = useElementSize();
-    return (
-        <div ref={ref} className="w-100 h-100">
-            <ErrorBoundary resetKeys={[orgUnits, program, periods]} FallbackComponent={ErrorFallback}>
-                <Table height={height}/>
-            </ErrorBoundary>
-        </div>
-    )
+  const { orgUnits, program, periods } = useDimension()
+  const [ref, { height }] = useElementSize()
+  return (
+    <div
+      ref={ref}
+      className="w-100 h-100"
+    >
+      <ErrorBoundary
+        resetKeys={[orgUnits, program, periods]}
+        FallbackComponent={ErrorFallback}
+      >
+        <Table height={height} />
+      </ErrorBoundary>
+    </div>
+  )
 }
 
-function Table({height}: { height: number }) {
-    const {data: rows, loading, pagination, error, columns, refetch, sortState, onSort} = useTableData();
-    const {dimensionsNotSelected, program} = useDimension();
-    const navigate = useNavigate();
+function Table({ height }: { height: number }) {
+  const {
+    data: rows,
+    loading,
+    pagination,
+    error,
+    columns,
+    refetch,
+    sortState,
+    onSort,
+  } = useTableData()
+  const { dimensionsNotSelected, program } = useDimension()
+  const navigate = useNavigate()
 
-    const onRowClick = (id: string) => {
-        navigate(`view/${id}?program=${program}`);
-    }
+  const onRowClick = (id: string) => {
+    navigate(`view/${id}?program=${program}`)
+  }
 
-    if (isEmpty(rows) && loading) {
-
-        return (
-            <ErrorBoundary onReset={() => {
-                refetch()
-            }} FallbackComponent={ErrorFallback}>
-                <ErrorThrower error={error}/>
-                <div className="column w-100 h-100 center align-center">
-                    <CircularLoader small/>
-                </div>
-            </ErrorBoundary>
-        )
-    }
-
-    if (isEmpty(rows)) {
-        return <EmptyList
-            message={dimensionsNotSelected ?
-                i18n.t("Select intervention, organisation unit, and period to start") :
-                i18n.t("There are no results for the selected intervention, period, and organisation unit")}
-        />
-    }
-
-    if (error) {
-        throw error;
-    }
-
+  if (isEmpty(rows) && loading) {
     return (
-        <div style={{padding: "0 16px", flex: 1, margin: "32px 0"}}
-             className=" w-100">
-            <CustomDataTable
-                onRowClick={onRowClick}
-                onSort={onSort}
-                sortState={sortState}
-                pagination={pagination}
-                loading={loading}
-                columns={columns}
-                rows={rows}
-                tableProps={{
-                    scrollHeight: `${height - 80}px`
-                }}
-            />
+      <ErrorBoundary
+        onReset={() => {
+          refetch()
+        }}
+        FallbackComponent={ErrorFallback}
+      >
+        <ErrorThrower error={error} />
+        <div className="column w-100 h-100 center align-center">
+          <CircularLoader small />
         </div>
+      </ErrorBoundary>
     )
+  }
+
+  if (isEmpty(rows)) {
+    return (
+      <EmptyList
+        message={
+          dimensionsNotSelected
+            ? i18n.t(
+                "Select intervention, organisation unit, and period to start"
+              )
+            : i18n.t(
+                "There are no results for the selected intervention, period, and organisation unit"
+              )
+        }
+      />
+    )
+  }
+
+  if (error) {
+    throw error
+  }
+
+  return (
+    <div
+      style={{ padding: "0 16px", flex: 1, margin: "32px 0" }}
+      className=" w-100"
+    >
+      <CustomDataTable
+        onRowClick={onRowClick}
+        onSort={onSort}
+        sortState={sortState}
+        pagination={pagination}
+        loading={loading}
+        columns={columns}
+        rows={rows}
+        tableProps={{
+          scrollHeight: `${height - 80}px`,
+        }}
+      />
+    </div>
+  )
 }
--- src/shared/components/Loaders/index.tsx
+++ src/shared/components/Loaders/index.tsx
@@ -1,45 +1,51 @@
-import React from 'react';
-import {CenteredContent, CircularLoader, LinearLoader} from '@dhis2/ui';
+import React from "react"
+import { CenteredContent, CircularLoader, LinearLoader } from "@dhis2/ui"
 
 export default function FullPageLoader({
-                                           minHeight,
-                                           message,
-                                       }: {
-    minHeight?: number | string;
-    message?: string;
+  minHeight,
+  message,
+}: {
+  minHeight?: number | string
+  message?: string
 }): React.ReactElement {
-    return (
-        <div style={{minHeight}} className="column w-100 h-100 align-center center ">
-            <CircularLoader small/>
-            {message && <p>{message}</p>}
-        </div>
-    );
+  return (
+    <div
+      style={{ minHeight }}
+      className="column w-100 h-100 align-center center "
+    >
+      <CircularLoader small />
+      {message && <p>{message}</p>}
+    </div>
+  )
 }
 
 export function FullPageProgressLoader({
-                                           minHeight,
-                                           message,
-                                           percentage
-                                       }: {
-    minHeight?: number | string;
-    message?: string;
-    percentage: number;
+  minHeight,
+  message,
+  percentage,
+}: {
+  minHeight?: number | string
+  message?: string
+  percentage: number
 }): React.ReactElement {
-    return (
-        <div style={{minHeight}} className="column w-100 h-100 align-center center ">
-            <div className="row gap-16 align-center">
-                <LinearLoader amount={percentage}/>
-                <h6 style={{margin: 0}}>{`${percentage}%`}</h6>
-            </div>
-            {message && <p>{message}</p>}
-        </div>
-    );
+  return (
+    <div
+      style={{ minHeight }}
+      className="column w-100 h-100 align-center center "
+    >
+      <div className="row gap-16 align-center">
+        <LinearLoader amount={percentage} />
+        <h6 style={{ margin: 0 }}>{`${percentage}%`}</h6>
+      </div>
+      {message && <p>{message}</p>}
+    </div>
+  )
 }
 
 export function CardLoader() {
-    return (
-        <CenteredContent>
-            <CircularLoader small/>
-        </CenteredContent>
-    );
+  return (
+    <CenteredContent>
+      <CircularLoader small />
+    </CenteredContent>
+  )
 }
