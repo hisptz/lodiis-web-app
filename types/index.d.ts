de88b6bf05804d50633382dfd1270ac2c7e05cfe
Gift Nnko <nnkogift@hotmail.com>
2024-02-22 15:19:15 +0300

    fix(reports): Fixes
    - Fixed issues with report download progress

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,9 @@
-declare module '@dhis2/d2-i18n';
-declare module '@dhis2/ui';
-declare module '*.css';
-declare module '*.svg';
-declare module '*.png';
-declare module '*.webp';
+declare module "*.css";
+declare module "*.svg";
+declare module "*.png";
+declare module "*.webp";
 declare module "async-es" {
-    export * from "async";
+	export * from "async";
 }
-declare module 'html2pdf.js';
-declare module "file-saver"
+declare module "html2pdf.js";
+declare module "file-saver";
